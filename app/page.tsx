6cfdcfc7497bf7e1e01e836431aedc1ad2332247
MITHILESH143 <66287130+MITHILESH143@users.noreply.github.com>
2025-11-07 07:38:00 +0000

    file is updated

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (<>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is texting page how to upload the file, in a backend and convert into the database.</p>
+    </>
+  );
+}
